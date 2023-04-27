f1d3dc4da3be08a838c81562acddf855fbe4cc31
TomsSiemaskevics <128472372+TomsSiemaskevics@users.noreply.github.com>
2023-04-27 11:43:56 +0300

    Initial Commit

---
+++ myWeatherAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myWeatherAPI
+//
+//  Created by toms.siemaskevics on 27/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
